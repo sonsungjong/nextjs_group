5fdd6d17be9e1b4c79b0457f3a6cf5a64995b116
sonsungjong <tbxmtbfm@naver.com>
2024-07-06 11:34:19 +0900

    css slug 업로드

--- src/app/group/[slug]/page.js
+++ src/app/group/[slug]/page.js
@@ -1,11 +1,35 @@
+import { connectDB } from "@/util/db"
+import { ObjectId } from "mongodb";
+import styles from './page.module.css';
+import Image from "next/image";
 
-export default function GroupPostPage({params}){
+export default async function GroupPostPage({params}){
+
+    // slug : 몽고DB의 _id
+    // 자세히보기 버튼이 /group/${_id}
+    const db = (await connectDB).db('mydb');
+    const activity = await db.collection('group').findOne({_id:ObjectId.createFromHexString(params.slug)});
+    console.log(activity);
+
     return(
         <div>
-            <h1>게시글 상세페이지</h1>
-            
-            {/* 내가 접속한 동적URL : params.폴더명 */}
-            <p>{params.slug}</p>
+            <header className={styles.header}>
+                <div className={styles.image}>
+                    <Image src={activity.image} alt={activity.title} fill/>
+                </div>
+                <div className={styles.headerText}>
+                    <h1>{activity.title}</h1>
+                    <p className={styles.creator}>
+                        작성자: {activity.creator}
+                    </p>
+                    <p className={styles.summary}>
+                        {activity.summary}
+                    </p>
+                </div>
+            </header>
+            <main>
+                <p dangerouslySetInnerHTML={{__html:activity.content}} className={styles.content}></p>
+            </main>
         </div>
     )
 }
